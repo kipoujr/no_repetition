e0719a1c190b18a325aab4a6342ac5c48a08f013
Kipoujr <kipoujr@hotmail.com>
2022-07-15 16:35:39 +0200

    Add files via upload

--- timing-experiments.sh
+++ timing-experiments.sh
@@ -8,21 +8,4 @@ for i in 1_4 2 4 7_1 8; do
     ./run.sh $i SAMPLING 0 STRUCTURED 1000000000 800000 10 1 0
 	echo -e "Starting $i-sampling with n=5000000, k=800000, experiments=10 (median trick)\n"   
     ./run.sh $i SAMPLING 1 STRUCTURED 5000000 800000 10 1 0
-
-	
-
-	echo -e "Starting $i-sampling on real data processed with n=648039, k=28000, experiments=10\n"    
-    ./run.sh $i SAMPLING 0 REAL_PROCESSED 648039 28000 10 1 0
-    echo -e "Starting $i-sampling-median on real data processed with n=648039, k=28000, experiments=10\n" 
-    ./run.sh $i SAMPLING 1 REAL_PROCESSED 648039 28000 10 1 0
-
-	echo -e "Starting $i-bottom-k on real data unprocessed with n=12775, k=7000, experiments=10\n"  
-    ./run.sh $i BOTTOM_K 0 REAL2 12775 7000 10 1 0
-    echo -e "Starting $i-bottom-k-median on real data unprocessed with n=12775, k=7000, experiments=10\n"   
-    ./run.sh $i BOTTOM_K 1 REAL2 12775 7000 10 1 0
-	
-	echo -e "Starting $i-sampling on real data unprocessed with n=20000, k=7000, experiments=10\n"    
-    ./run.sh $i SAMPLING 0 ATMOSPHERIC 20000 7000 10 1 0
-    echo -e "Starting $i-sampling-median on real data unprocessed with n=20000, k=7000, experiments=10\n"  
-    ./run.sh $i SAMPLING 1 ATMOSPHERIC 20000 7000 10 1 0
 done
