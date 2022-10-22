79e44f18cd32cc7b347e0b66aa6e9826374a1cee
kipoujr <kipoujr@hotmail.com>
2022-10-22 21:02:07 +0200

    Updated scripts (and figures) to output larger font text in figures

---
+++ scripts/accuracy.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from math import log10 , floor
+from scipy.stats import moment
+import os
+
+fontsz = 14
+datafiles = [a for a in os.listdir() if a.endswith('.txt') and not(a.startswith('time'))]
+n = len(datafiles)
+print(datafiles)
+
+data = {}
+highMoment = {}
+pos = [0,2,3,1,4]
+pos1 = [0,3,1,2,4]
+
+plt.figure(figsize=(6.3,3))
+for i, file in enumerate(datafiles):
+    with open(file) as f:
+        data[file] = np.array([int(line) for line in f.readlines()])
+        m = np.mean(data[file])
+        data[file] = (data[file]-m)/m
+        #print( '{0.0e}'.format(moment(data[file], moment=6)) )
+        x = moment(data[file], moment=6)
+        highMoment[i] = round(x, - int(floor(log10(abs(x)))))
+
+for i in range(n):
+    dat = data[datafiles[pos1[i]]]
+    #print(dat.shape)
+    #print(datafiles[pos1[i]])
+    plt.scatter(dat, np.full_like(dat, fill_value=i), color='black', alpha=0.3, label=datafiles[pos1[i]][:-4])
+
+plt.tick_params(axis='x', which='major', labelsize=fontsz)
+plt.locator_params(axis='x', nbins=6)
+plt.yticks(pos, ['Tab1Perm\n' + str(highMoment[0]),
+                 'Poly2\n' + str(highMoment[1]),
+                 'MultShift\n' + str(highMoment[2]),
+                 'MixedTab\n' + str(highMoment[3]),
+                 'Murmur\n' + str(highMoment[4])],
+           fontsize=fontsz)
+
+plt.axvline(x=min(min(data[datafiles[pos1[0]]]), min(data[datafiles[pos1[1]]])))
+plt.axvline(x=max(max(data[datafiles[pos1[0]]]), max(data[datafiles[pos1[1]]])))
+plt.savefig('pic.png',bbox_inches='tight')
--- scripts/timesPlot.py
+++ scripts/timesPlot.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-A = [357482,128529,199340,185371,115872]
-B = [293458,102255,204400,203542,108408]
+#A = [357482,128529,199340,185371,115872]
+#B = [293458,102255,204400,203542,108408]
 #A = [1892135,385659,1070399,188793,105579]
 #B = [1747741,582530,1112823,253676,122928]
 #A = [5279359,1409705,2829316,3250718,1958279]
 #B = [5349699,1235807,2731691,3023679,1781945]
-#A = [105663,35750,71442,25492,19723]
-#B = [87230,33720,64928,23890,17363]
+A = [105663,35750,71442,25492,19723]
+B = [87230,33720,64928,23890,17363]
 
+fontsz = 14
 performance  = [x/10**4 for x in A]
 print(performance)
 performance2 = [x/10**4 for x in B]
@@ -17,14 +18,14 @@ performance2 = [x/10**4 for x in B]
 plt.rcdefaults()
 fig, ax = plt.subplots()
 y_pos = np.arange(len(A))
-ax.set_xlabel('Average Running Time (ms)')
+ax.set_xlabel('Average Running Time (ms)', fontsize=fontsz)
 ax.set_yticks(y_pos)
 ax.set_yticklabels(['Murmur', 'Mult\nShift', 'Poly2', 'Mixed\nTab', 'Tab1\nPerm'])
 ax.invert_yaxis()  # labels read top-to-bottom
-ax.tick_params(axis='both', which='major', labelsize=13)
+ax.tick_params(axis='both', which='major', labelsize=fontsz)
 
 ax.barh(y_pos-0.2, performance , 0.4, label='PC 1')
 ax.barh(y_pos+0.2, performance2, 0.4, label='PC 2')
-ax.legend()
+ax.legend(fontsize=fontsz)
 
-plt.savefig('pic.png')
+plt.savefig('pic.png',bbox_inches='tight')
